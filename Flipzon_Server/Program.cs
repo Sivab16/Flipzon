ce70dfe3bc5bc2da96e4e1fe03ae2cf0c995fc39
Sivaprakash <sivasoftinfo@gmail.com>
2024-06-25 11:49:40 +0530

    1.Blazor Webassembly project added 2.Class library project added for models 3.Product component added 4.New product menu added in the nav link 5.Project reference include in the Imports component

--- Flipzon_Server/Program.cs
+++ Flipzon_Server/Program.cs
@@ -24,14 +24,15 @@ namespace Flipzon_Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+         
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseRouting(); // assign the endpoint for our application
 
-            app.MapBlazorHub();
+            app.MapBlazorHub(); 
+            // SignalR communicaiton is works here
             app.MapFallbackToPage("/_Host");
 
             app.Run();
